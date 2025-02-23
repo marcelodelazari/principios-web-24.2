f30e616f59588c36112a076add43137ab3391268
marcelodelazari <marceloalvesdelazari@gmail.com>
2025-02-23 20:05:27 -0300

    Revert "CRUD de usuário completo."
    This reverts commit 0240f8964a72ff454ca4116e4cb5bac78cdb3c75.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import userRouter from './user';
+import authRouter from './auth';
 import postsRouter from './posts';
 
 const router = Router();
 
-router.use(userRouter);
+router.use(authRouter);
 router.use(postsRouter);
 
 export default router;
