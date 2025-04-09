d045bee3d1a820a196cca444ccef063e198aff45
marcelodelazari <marceloalvesdelazari@gmail.com>
2025-04-08 21:00:20 -0300

    adicionando log para debug

--- backend/src/config/supabase.ts
+++ backend/src/config/supabase.ts
@@ -3,6 +3,9 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.SUPABASE_URL || "";
 const supabaseKey = process.env.SUPABASE_KEY || "";
 
+console.log("SUPABASE_URL:", supabaseUrl);
+console.log("SUPABASE_KEY:", supabaseKey);
+
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("SUPABASE_URL ou SUPABASE_KEY não configurados no .env");
 }
